7275011f47285249237dddc996c92bfce461ec8a
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-12-07 17:06:01 +0900

    typo🤣

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import errorMassage from "./rules/error-massage-is-not-error-message";
 
 export = {
   rules: {
-    "error-masasge-is-not-error-message": errorMassage,
+    "error-massage": errorMassage,
   },
 };
